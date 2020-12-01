e0ef34a36323548a6b8d1be350ead69beaa92415
AlonMiz <alon@logz.io>
2020-12-01 21:32:24 +0200

    Update Lodash to mute github

--- __tests__/one-version-rule.spec.js
+++ __tests__/one-version-rule.spec.js
@@ -5,7 +5,7 @@ describe('One Version Rule', function () {
   it('should get all dependencies versions', function () {
     const allDeps = getAllDependenciesVersions({ rootDir: path.resolve(__dirname) })
     expect(allDeps).toMatchObject({
-      "lodash": { "dep": "lodash", "versions": ["4.17.0"] },
+      "lodash": { "dep": "lodash", "versions": ["4.17.20"] },
       "moment": { "dep": "moment", "haveConflicts": true, "versions": ["2.28.0", "2.29.0"] },
       "react": { "dep": "react", "versions": ["17.0.0"] },
       "axios": { "dep": "axios", "versions": ["0.21.0"] }
